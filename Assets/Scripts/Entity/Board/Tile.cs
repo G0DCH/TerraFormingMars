ae3f105fa69ccc71844603675eb89b2f4aadeef0
G0DCH <45649338+G0DCH@users.noreply.github.com>
2020-02-20 19:39:21 +0900

    타일 설정 끗

--- Assets/Scripts/Entity/Board/Tile.cs
+++ Assets/Scripts/Entity/Board/Tile.cs
@@ -11,7 +11,13 @@ namespace TerraFormmingMars.Entity.Board
         private TileData myTileData;
         public TileData MyTileData { get { return myTileData; } }
 
+        [SerializeField]
         private List<Tile> nearTiles;
+
+        private void Start()
+        {
+            InitNearTiles();
+        }
 
         public int GetScore()
         {
@@ -24,14 +30,39 @@ namespace TerraFormmingMars.Entity.Board
             else if(MyTileData.TileType == TileType.City)
             {
                 //주변의 나무 1개마다 1점씩 추가
+                foreach(Tile nearTile in nearTiles)
+                {
+                    if(nearTile.MyTileData.TileType == TileType.Tree)
+                    {
+                        score++;
+                    }
+                }
             }
             else if(MyTileData.TileType == TileType.Capital)
             {
                 //주변의 물과 나무 1개마다 1점씩 추가
+                foreach (Tile nearTile in nearTiles)
+                {
+                    if (nearTile.MyTileData.TileType == TileType.Tree)
+                    {
+                        score++;
+                    }
+                    else if(nearTile.MyTileData.TileType == TileType.Ocean)
+                    {
+                        score++;
+                    }
+                }
             }
             else if(MyTileData.TileType == TileType.Market)
             {
                 //주변의 도시 1개 마다 1점씩 추가
+                foreach (Tile nearTile in nearTiles)
+                {
+                    if (nearTile.MyTileData.TileType == TileType.City)
+                    {
+                        score++;
+                    }
+                }
             }
 
             return score;
@@ -44,6 +75,116 @@ namespace TerraFormmingMars.Entity.Board
 
             int lineNum = int.Parse(lineName[4].ToString());
             int tileNum = int.Parse(myName[4].ToString());
+
+            const string LINE = "Line";
+            const string TILE = "Tile";
+
+            //위 아래 추가
+            if(lineNum <5)
+            {
+                if(lineNum != 1)
+                {
+                    //위에 라인 추가
+                    GameObject upperLine = GameObject.Find(LINE + (lineNum - 1).ToString());
+
+                    //타일 번호가 1번이나 라인의 마지막 번호가 아니면 두개 추가
+                    if (tileNum != 1 && tileNum != transform.parent.childCount)
+                    {                        
+                        nearTiles.Add(upperLine.transform.Find(TILE + (tileNum - 1).ToString()).GetComponent<Tile>());
+                        nearTiles.Add(upperLine.transform.Find(TILE + tileNum.ToString()).GetComponent<Tile>());
+                    }
+                    else if(tileNum == 1)
+                    {
+                        nearTiles.Add(upperLine.transform.Find(TILE + tileNum.ToString()).GetComponent<Tile>());
+                    }
+                    else if(tileNum == transform.parent.childCount)
+                    {
+                        nearTiles.Add(upperLine.transform.Find(TILE + (tileNum - 1).ToString()).GetComponent<Tile>());
+                    }
+                }
+                
+                //아래 라인 추가
+                GameObject lowerLine = GameObject.Find(LINE + (lineNum + 1).ToString());
+
+                nearTiles.Add(lowerLine.transform.Find(TILE + (tileNum + 1).ToString()).GetComponent<Tile>());
+                nearTiles.Add(lowerLine.transform.Find(TILE + tileNum.ToString()).GetComponent<Tile>());
+            }
+            else if(lineNum == 5)
+            {
+                //위에 라인 추가
+                GameObject upperLine = GameObject.Find(LINE + (lineNum - 1).ToString());
+
+                //아래 라인 추가
+                GameObject lowerLine = GameObject.Find(LINE + (lineNum + 1).ToString());
+
+                //타일 번호가 1번이나 라인의 마지막 번호가 아니면 두개 추가
+                if (tileNum != 1 && tileNum != transform.parent.childCount)
+                {
+                    nearTiles.Add(upperLine.transform.Find(TILE + (tileNum - 1).ToString()).GetComponent<Tile>());
+                    nearTiles.Add(upperLine.transform.Find(TILE + tileNum.ToString()).GetComponent<Tile>());
+
+                    nearTiles.Add(lowerLine.transform.Find(TILE + (tileNum - 1).ToString()).GetComponent<Tile>());
+                    nearTiles.Add(lowerLine.transform.Find(TILE + tileNum.ToString()).GetComponent<Tile>());
+                }
+                else if (tileNum == 1)
+                {
+                    nearTiles.Add(upperLine.transform.Find(TILE + tileNum.ToString()).GetComponent<Tile>());
+
+                    nearTiles.Add(upperLine.transform.Find(TILE + tileNum.ToString()).GetComponent<Tile>());
+                }
+                else if (tileNum == transform.parent.childCount)
+                {
+                    nearTiles.Add(upperLine.transform.Find(TILE + (tileNum - 1).ToString()).GetComponent<Tile>());
+
+                    nearTiles.Add(upperLine.transform.Find(TILE + (tileNum - 1).ToString()).GetComponent<Tile>());
+                }
+            }
+            else if(lineNum > 5)
+            {
+                if (lineNum != 9)
+                {
+                    //아래 라인 추가
+                    GameObject lowerLine = GameObject.Find(LINE + (lineNum + 1).ToString());
+
+                    //타일 번호가 1번이나 라인의 마지막 번호가 아니면 두개 추가
+                    if (tileNum != 1 && tileNum != transform.parent.childCount)
+                    {
+                        nearTiles.Add(lowerLine.transform.Find(TILE + (tileNum - 1).ToString()).GetComponent<Tile>());
+                        nearTiles.Add(lowerLine.transform.Find(TILE + tileNum.ToString()).GetComponent<Tile>());
+                    }
+                    else if (tileNum == 1)
+                    {
+                        nearTiles.Add(lowerLine.transform.Find(TILE + tileNum.ToString()).GetComponent<Tile>());
+                    }
+                    else if (tileNum == transform.parent.childCount)
+                    {
+                        nearTiles.Add(lowerLine.transform.Find(TILE + (tileNum - 1).ToString()).GetComponent<Tile>());
+                    }
+                }
+
+                //위에 라인 추가
+                GameObject upperLine = GameObject.Find(LINE + (lineNum - 1).ToString());
+
+                nearTiles.Add(upperLine.transform.Find(TILE + (tileNum + 1).ToString()).GetComponent<Tile>());
+                nearTiles.Add(upperLine.transform.Find(TILE + tileNum.ToString()).GetComponent<Tile>());
+            }
+
+            //위 아래 끝 좌우 추가 시작
+
+            //타일 번호가 1번이나 라인의 맨 끝 번호가 아니라면
+            if(tileNum != 1 && tileNum != transform.parent.childCount)
+            {
+                nearTiles.Add(transform.parent.Find(TILE + (tileNum - 1).ToString()).GetComponent<Tile>());
+                nearTiles.Add(transform.parent.Find(TILE + (tileNum + 1).ToString()).GetComponent<Tile>());
+            }
+            else if(tileNum == 1)
+            {
+                nearTiles.Add(transform.parent.Find(TILE + (tileNum + 1).ToString()).GetComponent<Tile>());
+            }
+            else if(tileNum == transform.parent.childCount)
+            {
+                nearTiles.Add(transform.parent.Find(TILE + (tileNum - 1).ToString()).GetComponent<Tile>());
+            }
         }
     }
 }
