2db46b78698a73c9f55f7b20e2113215efa84108
G0DCH <45649338+G0DCH@users.noreply.github.com>
2020-03-01 13:30:45 +0900

    카드 사용 테스트

--- Assets/Scripts/Logics/Manager/CardManager.cs
+++ Assets/Scripts/Logics/Manager/CardManager.cs
@@ -16,9 +16,37 @@ namespace TerraFormmingMars.Logics.Manager
         public void UseCard(CardData cardData)
         {
             //제한 검사
+            if(LimitFunctionManager.Instance.CheckLimit(cardData.LimitFunction) == false)
+            {
+                return;
+            }
             //비용 검사
+            if(CheckCost(cardData) == false)
+            {
+                return;
+            }
             //함수 실행
             //비용 지불
+        }
+
+        private bool CheckCost(CardData cardData)
+        {
+            int cost = cardData.Cost;
+
+            Entity.Source source;
+            if(PlayerManager.Instance.TurnPlayer.TypeSourceMap.TryGetValue(SourceType.MC, out source))
+            {
+                if(source.Amount >= cost)
+                {
+                    return true;
+                }
+            }
+            else
+            {
+                Debug.LogError(source.SourceType + "에 해당하는 자원이 없습니다.");
+            }
+
+            return false;
         }
     }
 }
--- Assets/Scripts/Logics/Manager/LimitFunctionManager.cs
+++ Assets/Scripts/Logics/Manager/LimitFunctionManager.cs
@@ -10,6 +10,13 @@ namespace TerraFormmingMars.Logics.Manager
 {
     public class LimitFunctionManager : Singleton<LimitFunctionManager>
     {
+        public Entity.Card.CardData cardData;
+
+        private void Start()
+        {
+            CheckLimit(cardData.LimitFunction);
+        }
+
         /// <summary>
         /// FunctionData와 일치하는 조건 검사 함수 실행
         /// </summary>
@@ -18,15 +25,30 @@ namespace TerraFormmingMars.Logics.Manager
         public bool CheckLimit(FunctionData functionData)
         {
             Type type = GetType();
-            MethodInfo functionInfo = 
+            MethodInfo functionInfo =
                 type.GetMethod(functionData.FunctionName, BindingFlags.Instance | BindingFlags.NonPublic);
-            functionInfo.Invoke(this, functionData.FunctionArguments.ToArray());
 
-            return false;
+            return (bool)functionInfo.Invoke(this, functionData.FunctionArguments.ToArray());
         }
 
         private bool ProductLimit(params string[] arguments)
         {
+            string sourceType = arguments[0];
+            int productLimit = int.Parse(arguments[1]);
+
+            Source source;
+            if (PlayerManager.Instance.TurnPlayer.StringSourceMap.TryGetValue(sourceType, out source))
+            {
+                if (source.Product >= productLimit)
+                {
+                    return true;
+                }
+            }
+            else
+            {
+                Debug.LogError(sourceType + "에 해당하는 자원이 없습니다.");
+            }
+
             return false;
         }
     }
